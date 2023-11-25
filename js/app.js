edfbaf1b7827dfaf32e9a01e9971140ab11f7f4b
= <=>
2023-11-25 20:42:42 +0530

    helper methods added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-// Hamburger icon click animation
+// Hamburger icon click animation and sidemenu view
 function toggleMenu(element) {
     element.classList.toggle("change");
+
+    var sidebar = document.getElementById('mySidebar');
+    sidebar.style.width = sidebar.style.width === '200px' ? '0' : '200px';
 }
