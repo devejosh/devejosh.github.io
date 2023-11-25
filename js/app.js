98fe93412028d9f316e3a9444de68e96e5769d7e
= <=>
2023-11-25 21:16:49 +0530

    implemented the modal at page load

--- js/app.js
+++ js/app.js
@@ -5,3 +5,20 @@ function toggleMenu(element) {
     var sidebar = document.getElementById('mySidebar');
     sidebar.style.width = sidebar.style.width === '200px' ? '0' : '200px';
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the modal element
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var modalButton = document.getElementById('closeModal');
+
+    // Show the modal when the page loads
+    modal.style.display = 'block';
+
+    // Close the modal when the close button is clicked
+    modalButton.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+});
