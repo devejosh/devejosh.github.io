8ce672777576247f165e1248e830f68cd1b3650a
= <=>
2024-05-05 04:30:29 +0530

    fixed broken links

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 // Hamburger icon click animation and sidemenu view
 function toggleMenu(element) {
     element.classList.toggle("change");
-
-    var sidebar = document.getElementById('mySidebar');
-    sidebar.style.width = sidebar.style.width === '200px' ? '0' : '200px';
 }
 
 
